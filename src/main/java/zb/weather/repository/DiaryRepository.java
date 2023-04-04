c43da01420839891b869e376042704357c0253bf
datadbs <dj9034kr@gmail.com>
2023-04-04 21:46:58 +0900

    feat: add weather Diary CRUD

--- src/main/java/zb/weather/repository/DiaryRepository.java
+++ src/main/java/zb/weather/repository/DiaryRepository.java
@@ -2,8 +2,19 @@ package zb.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import zb.weather.domain.Diary;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
+    List<Diary> findAllByDate(LocalDate date);
+    List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Diary getFirstByDate(LocalDate date);
+    @Transactional
+    void deleteAllByDate(LocalDate date);
 }
+
