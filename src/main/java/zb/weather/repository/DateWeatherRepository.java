5c44b52d623ae3cbdc2b44f89a002208589751ae
datadbs <dj9034kr@gmail.com>
2023-04-05 16:20:08 +0900

    feat: add cache

---
+++ src/main/java/zb/weather/repository/DateWeatherRepository.java
@@ -0,0 +1,13 @@
+package zb.weather.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import zb.weather.domain.DateWeather;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+    List<DateWeather> findAllByDate(LocalDate date);
+}
--- src/main/java/zb/weather/service/DiaryService.java
+++ src/main/java/zb/weather/service/DiaryService.java
@@ -5,10 +5,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import zb.weather.domain.DateWeather;
 import zb.weather.domain.Diary;
+import zb.weather.repository.DateWeatherRepository;
 import zb.weather.repository.DiaryRepository;
 
 import java.io.BufferedReader;
@@ -27,27 +30,62 @@ public class DiaryService {
     @Value("${openweathermap.key}")
     private String apiKey;
     private final DiaryRepository diaryRepository;
-
-    public DiaryService(DiaryRepository diaryRepository) {
+    private final DateWeatherRepository dateWeatherRepository;
+    public DiaryService(DiaryRepository diaryRepository, DateWeatherRepository dateWeatherRepository) {
         this.diaryRepository = diaryRepository;
+        this.dateWeatherRepository = dateWeatherRepository;
     }
 
+    @Transactional
+    @Scheduled(cron = "0 0 1 * * *")
+    public void saveWeatherDate(){
+        dateWeatherRepository.save(getWeatherFromApi());
+    }
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public void  createDiary(LocalDate date, String text){
+        //날씨 데이터 가져오기 (API 에서 가져오기? or DB에서 가져오기?)
+        //api에서 가져오기
+        //open weather map에서 날씨 데이터 가져오기
+//        String weatherData = getWeatherString();
+        //받아온 날씨 json 파싱하기
+//        Map<String, Object> parsedWeather = parseWeather(weatherData);
+        //DB에서 가져오기
+        DateWeather dateWeather = getDateWeather(date);
+        //파싱된 데이터 + 일기 값 우리 db에 넣기
+        Diary nowDiary = new Diary();
+//        nowDiary.setDate(date);
+//        nowDiary.setWeather(parsedWeather.get("main").toString());
+//        nowDiary.setIcon(parsedWeather.get("icon").toString());
+//        nowDiary.setTemperature((Double) parsedWeather.get("temp"));
+        nowDiary.setDateWeather(dateWeather);
+        nowDiary.setText(text);
+        diaryRepository.save(nowDiary);
+    }
+
+    private DateWeather getWeatherFromApi(){
         //open weather map에서 날씨 데이터 가져오기
         String weatherData = getWeatherString();
 
         //받아온 날씨 json 파싱하기
         Map<String, Object> parsedWeather = parseWeather(weatherData);
+        DateWeather dateWeather = new DateWeather();
+        dateWeather.setDate(LocalDate.now());
+        dateWeather.setWeather(parsedWeather.get("main").toString());
+        dateWeather.setIcon(parsedWeather.get("icon").toString());
+        dateWeather.setTemperature((Double) parsedWeather.get("temp"));
+        return dateWeather;
 
-        //파싱된 데이터 + 일기 값 우리 db에 넣기
-        Diary nowDiary = new Diary();
-        nowDiary.setWeather(parsedWeather.get("main").toString());
-        nowDiary.setIcon(parsedWeather.get("icon").toString());
-        nowDiary.setTemperature((Double) parsedWeather.get("temp"));
-        nowDiary.setText(text);
-        nowDiary.setDate(date);
-        diaryRepository.save(nowDiary);
+    }
+
+    private DateWeather getDateWeather(LocalDate date){
+        List<DateWeather> dateWeatherListFromDB = dateWeatherRepository.findAllByDate(date);
+        if(dateWeatherListFromDB.size() == 0){
+            //새로 api에서 날씨 정보를 가져와야한다
+            //정책상,,, 현재 날씨를 가져오도록 하거나,,, 날씨없이 일기를 쓰도록,,,
+            return getWeatherFromApi();
+        } else {
+            return dateWeatherListFromDB.get(0);
+        }
     }
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date){
